44f32e656fc4f3acd1230810975a1a3663e61009
Greg Banks <gnb@fastmail.fm>
2011-03-24 15:51:17 +1100

    Report external name for FOLDER fetch item.
    Also, handle folder names with odd characters properly, by reporting the
    name as the simplest of an atom, a quoted string, or a literal.

--- imap/imapd.c
+++ imap/imapd.c
@@ -4377,6 +4377,11 @@ static int parse_fetch_args(const char *tag, const char *cmd,
 	}
     }
 
+    if (fetchitems & FETCH_FOLDER) {
+	fetchargs.namespace = &imapd_namespace;
+	fetchargs.userid = imapd_userid;
+    }
+
     *fa = fetchargs;
     fa->fetchitems = fetchitems;
     strarray_free(newfields);
