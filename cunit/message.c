beae0b1bacb776cc4d2fa706581484e212aea1fa
Greg Banks <gnb@fastmail.fm>
2011-06-21 12:24:32 +1000

    unit: fix more Valgrind problems
    Another instance of the problem where CU_ASSERT_STRING_EQUALS
    doesn't handle literals containing % characters.

--- cunit/message.c
+++ cunit/message.c
@@ -628,7 +628,7 @@ static void test_references(void)
     message_write_nstring(&b, input); \
     buf_cstring(&b); \
     CU_ASSERT_EQUAL(b.len, explen); \
-    CU_ASSERT_STRING_EQUAL(b.s, expout)
+    CU_ASSERT(!strcmp(b.s, expout))
 
 static void test_write_nstring(void)
 {
