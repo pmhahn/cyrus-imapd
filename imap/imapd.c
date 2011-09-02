fe09c03fece624b3cf97eab35acf946acb79ca62
Greg Banks <gnb@fastmail.fm>
2011-09-02 13:46:57 +1000

    Report available quota resources in capabilities
    Using X-QUOTA=name for each resource name supported.  I don't see a
    standardised way of reporting to the client which quota resources are
    supported by the server, in particular RFC 2087 is silent on this.

--- imap/imapd.c
+++ imap/imapd.c
@@ -2921,6 +2921,9 @@ void capa_response(int flags)
     }
 #endif
 
+    for (i = 0 ; i < QUOTA_NUMRESOURCES ; i++)
+	prot_printf(imapd_out, " X-QUOTA=%s", quota_names[i]);
+
     if (idle_enabled()) {
 	prot_printf(imapd_out, " IDLE");
     }
