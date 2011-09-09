e4f7dbc640585b85fa8eea3c3247471074327063
Greg Banks <gnb@fastmail.fm>
2011-09-09 21:49:05 +1000

    annotate: use mailbox* instead of name in API
    The annotate_state_t now always has a mailbox* open when used in
    per-message or per-mailbox scope.  This required moving the traversal of
    the mboxlist database to implement mailbox name patterns out of the API
    and into the caller (although a helper annotate_apply_mailboxes() is
    supplied to make it easier).
    
    This has some implications.
    
    Some of the internals of annotate_state_t needed reorganisation, which
    impacted a lot of code.  Because a mailbox* is always open, the various
    set/get functions which needed to do things to an open mailbox* are now
    simpler.  Converesely however, we don't always have an mbentry and so
    might need to do an extra mboxlist lookup (or more precisely, an
    mboxlist lookup later and in a different place).
    
    Because of the inversion of control flow, the operation of getting or
    setting many annotations on many folders will now do more looking up of
    entry names in the tables, which is now done once per matched folder
    instead of once per SETMETADATA command.
    
    The call to mboxlist_setspecialuse() is now double-opening a mailbox*
    which fortunately the mailbox API can handle just fine, but it's
    awkward.
    
    The cunit annotate tests now all open a real mailbox* instead of faking
    one.

--- imap/sync_server.c
+++ imap/sync_server.c
@@ -1819,6 +1819,7 @@ static int do_annotation(struct dlist *kin)
     struct buf value = BUF_INITIALIZER;
     const char *userid = NULL;
     char *name = NULL;
+    struct mailbox *mailbox = NULL;
     annotate_state_t *astate = NULL;
     int r;
 
@@ -1837,6 +1838,10 @@ static int do_annotation(struct dlist *kin)
     name = xstrdup(mboxname);
     mboxname_hiersep_toexternal(sync_namespacep, name, 0);
 
+    r = mailbox_open_iwl(name, &mailbox);
+    if (r)
+	goto done;
+
     appendattvalue(&attvalues,
 		   *userid ? "value.priv" : "value.shared",
 		   &value);
@@ -1844,7 +1849,7 @@ static int do_annotation(struct dlist *kin)
     astate = annotate_state_new();
     annotate_state_set_auth(astate, sync_namespacep,
 			    sync_userisadmin, userid, sync_authstate);
-    annotate_state_set_mailbox(astate, name);
+    annotate_state_set_mailbox(astate, mailbox);
 
     r = annotatemore_begin();
     if (!r)
@@ -1852,6 +1857,8 @@ static int do_annotation(struct dlist *kin)
     if (!r)
 	annotatemore_commit();
 
+done:
+    mailbox_close(&mailbox);
     freeentryatts(entryatts);
     free(name);
     annotate_state_free(&astate);
@@ -1868,6 +1875,7 @@ static int do_unannotation(struct dlist *kin)
     const char *userid = NULL;
     struct buf empty = BUF_INITIALIZER;
     char *name = NULL;
+    struct mailbox *mailbox = NULL;
     annotate_state_t *astate = NULL;
     int r;
 
@@ -1883,6 +1891,10 @@ static int do_unannotation(struct dlist *kin)
     name = xstrdup(mboxname);
     mboxname_hiersep_toexternal(sync_namespacep, name, 0);
 
+    r = mailbox_open_iwl(name, &mailbox);
+    if (r)
+	goto done;
+
     appendattvalue(&attvalues,
 		   *userid ? "value.priv" : "value.shared",
 		   &empty);
@@ -1890,7 +1902,7 @@ static int do_unannotation(struct dlist *kin)
     astate = annotate_state_new();
     annotate_state_set_auth(astate, sync_namespacep,
 			    sync_userisadmin, userid, sync_authstate);
-    annotate_state_set_mailbox(astate, name);
+    annotate_state_set_mailbox(astate, mailbox);
 
     r = annotatemore_begin();
     if (!r)
@@ -1898,6 +1910,8 @@ static int do_unannotation(struct dlist *kin)
     if (!r)
 	annotatemore_commit();
 
+done:
+    mailbox_close(&mailbox);
     freeentryatts(entryatts);
     free(name);
 
