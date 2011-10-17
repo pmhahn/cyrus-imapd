711af192abefdad5ae711726f7a6bf6dbcf8cfc9
Bron Gondwana <brong@opera.com>
2011-10-17 14:04:07 +0200

    mboxlist: fix set_specialuse ordering

--- imap/mboxlist.c
+++ imap/mboxlist.c
@@ -1792,12 +1792,7 @@ int mboxlist_setspecialuse(struct mailbox *mailbox, const char *specialuse)
 	goto done;
     }
 
-    r = mailbox_set_specialuse(mailbox, specialuse);
-    if (r) goto done;
-
-    r = mailbox_commit(mailbox);
-    if (r) goto done;
-
+    /* update the entry */
     mbentry->specialuse = specialuse;
     mboxent = mboxlist_entry_cstring(mbentry);
     do {
@@ -1811,6 +1806,13 @@ int mboxlist_setspecialuse(struct mailbox *mailbox, const char *specialuse)
 	r = IMAP_IOERROR;
 	goto done;
     }
+
+    /* update the mailbox and commit */
+    r = mailbox_set_specialuse(mailbox, specialuse);
+    if (r) goto done;
+
+    r = mailbox_commit(mailbox);
+    if (r) goto done;
 
     /* 3. Commit transaction */
     r = DB->commit(mbdb, tid);
